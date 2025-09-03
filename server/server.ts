c944b89d4d0c08be2ead789f47b3a4b21aaaf4d7
deekshith <deekshithm321@gmail.com>
2025-09-03 19:19:53 +0530

    mongoDB connected

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 //initialize express app
 const app = express()
+
+//connect to database
+await connectDB();
 
 //middleware
 app.use(cors());
@@ -13,7 +17,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Service Booking Platform API");
 });
 
-//routes here
+//routes
 
 //start the server
 const PORT = process.env.PORT || 5000;
